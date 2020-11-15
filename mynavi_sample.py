13d54a3042091e200923c0c9c50e8e6bd55a657d
hiroakiitou <pigaru.gooood.mar19@gmail.com>
2020-11-15 17:06:11 +0900

    2ページ目情報取得作成中

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -23,7 +23,7 @@ def set_driver(driver_path,headless_flg):
 
 ### main処理
 def main():
-    search_keyword="高収入"
+    search_keyword=input("求人情報検索ワードを入力してください >>> ")
     # driverを起動
     driver=set_driver("chromedriver.exe",False)
     # Webサイトを開く
@@ -39,19 +39,41 @@ def main():
     driver.find_element_by_class_name("topSearch__text").send_keys(search_keyword)
     # 検索ボタンクリック
     driver.find_element_by_class_name("topSearch__button").click()
-    
 
+    # 検索＆出力処理
+    get_site_info(driver)
+
+    # 次ページへのリンククリック
+    driver.find_element_by_class_name("iconFont--arrowLeft").click()
+
+    # 検索＆出力処理
+    get_site_info(driver)
+
+
+def get_site_info(driver):
     # 検索結果の一番上の会社名を取得
     name_list=driver.find_elements_by_class_name("cassetteRecruit__name")
     copy_list=driver.find_elements_by_class_name("cassetteRecruit__copy")
     status_list=driver.find_elements_by_class_name("labelEmploymentStatus")
+    # 複数データ取得項目
+    main_list=driver.find_elements_by_class_name("cassetteRecruit__main")
+
+    # 日付
+    update_date_list=driver.find_elements_by_class_name("cassetteRecruit__updateDate")
+    end_date_list=driver.find_elements_by_class_name("cassetteRecruit__endDate")
+
     # 1ページ分繰り返し
-    print("{},{},{}".format(len(copy_list),len(status_list),len(name_list)))
-    for name,copy,status in zip(name_list,copy_list,status_list):
+    print("{},{},{},{},{},{}".format(len(copy_list),len(status_list),len(name_list),len(main_list),len(update_date_list),len(end_date_list)))
+    for name,copy,status,main,updatedate,enddate in zip(name_list,copy_list,status_list,main_list,update_date_list,end_date_list):
         print(name.text)
         print(copy.text)
         print(status.text)
-
+        main_td_list = main.find_elements_by_class_name("tableCondition__body")
+        for main_td in main_td_list:
+           print(main_td.text)
+            
+        print(updatedate.text)
+        print(enddate.text)
 
 ### 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
 if __name__ == "__main__":
